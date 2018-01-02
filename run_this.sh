6541307395618980eb31bbee14babad2bd374b6e
Shaun Smiley <senorsmile@gmail.com>
2018-01-02 13:57:26 -0800

    add callback_whitelist and default to 2.4.1 git

--- run_this.sh
+++ run_this.sh
@@ -2,6 +2,13 @@
 export ANSIBLE_BASE_RUN_MODE='playbook'
 export ANSIBLE_PLAYBOOK='site.yml'
 
+export ANSIBLE_CALLBACK_WHITELIST='timer,profile_tasks'
+
+
+export USE_ANSIBLE_MODE=git
+export USE_ANSIBLE_VER='v2.4.1.0-1'
+#export USE_ANSIBLE_VER='v2.3.3.0-1'
+
 source base_run.sh
 
 ./base_run.sh "${@}"
