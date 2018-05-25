879e09734e644d8765d5e0230c2b4102b1b15356
Shaun Smiley <senorsmile@gmail.com>
2018-05-25 13:06:35 -0700

    update bootstrap.h for 18.04

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,25 +1,52 @@
 #!/usr/bin/env bash
 
-packages=(
-	## to get easy_install
-	software-properties-common 
-	python-software-properties
+which apt >/dev/null 2>&1 && {
+	grep '16.04' /etc/lsb-release >/dev/null 2>&1 && {
+		packages=(
+			## to get easy_install
+			software-properties-common 
+			python-software-properties
 
-	python-setuptools 
-	python-dev 
-	gcc
+			python-setuptools 
+			python-dev 
+			gcc
 
-	## to get this repo
-	git
+			## to get this repo
+			git
 
-	## nice to have
-	vim
-)
+			## nice to have
+			vim
+		)
+	}
+	
+	grep '18.04' /etc/lsb-release >/dev/null 2>&1 && {
+		packages=(
+			## python-pip and deps
+			software-properties-common 
+
+			python-setuptools 
+			python-pip
+			python-dev 
+			gcc
+
+			## to get this repo
+			git
+
+			## nice to have
+			vim
+			htop
+		)
+	}
+}
 
 pip_packages=(
 	pipenv
 )
 
-sudo apt-get install "${packages[@]}"
-sudo easy_install pip
+sudo apt-get -y install "${packages[@]}"
+
+grep '16.04' /etc/lsb-release >/dev/null 2>&1 && {
+	sudo easy_install pip
+}
+
 sudo pip install "${pip_packages[@]}"
